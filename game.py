e2f574b8befcd7aca8a26f19214da1630dbde05a
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2021-02-22 15:48:56 +0700

    reward system bug fixed

--- game.py
+++ game.py
@@ -142,8 +142,10 @@ class Overcooked:
                 if i_recipe_trim == recipe:
                     reward += recipe.layers * progress_rwd
                     incomplete.remove(i_recipe)
-
-        reward += deliver_rwd * self.count_completed()
+        
+        for recipe in self.deliver_counter.contains:
+            reward += recipe.layers * progress_rwd + deliver_rwd
+            
         return reward
     
 
